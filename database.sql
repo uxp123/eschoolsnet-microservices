0593487dc11926e01ba057c95843ad783b41f7ef
Papsy <57874831+uxp123@users.noreply.github.com>
2020-10-11 18:09:28 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: Oct 11, 2020 at 06:08 PM
--- Server version: 5.7.26
--- PHP Version: 7.4.2
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
